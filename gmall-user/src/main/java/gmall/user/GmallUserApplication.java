355b531641f9c51a410e229c9c17901bcb8cdbfc
txq <1353756406@qq.com>
2020-02-21 16:49:35 +0800

    "user model"

---
+++ gmall-user/src/main/java/gmall/user/GmallUserApplication.java
@@ -0,0 +1,15 @@
+package gmall.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("gmall.user.mapper")
+public class GmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserApplication.class, args);
+    }
+
+}
