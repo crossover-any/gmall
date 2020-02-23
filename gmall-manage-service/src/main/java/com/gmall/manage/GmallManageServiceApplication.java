2ba34fef29a33350560f15e745b4b0ca51e017bb
txq <1353756406@qq.com>
2020-02-23 19:21:32 +0800

    node.js Integration

---
+++ gmall-manage-service/src/main/java/com/gmall/manage/GmallManageServiceApplication.java
@@ -0,0 +1,15 @@
+package com.gmall.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.gmall.manage.mapper")
+public class GmallManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallManageServiceApplication.class, args);
+    }
+
+}
---
+++ gmall-manage-web/src/test/java/com/gmall/manage/GmallManageWebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gmall.manage;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
