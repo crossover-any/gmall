2403fc5390f2ccb4f6a27e1c37f4237fd7312af4
txq <1353756406@qq.com>
2020-03-06 22:17:04 +0800

    添加面包屑

---
+++ gmall-api/src/main/java/com/gmall/bean/PmsSearchCrumb.java
@@ -0,0 +1,41 @@
+package com.gmall.bean;
+
+/**
+ * @Author Tengxq
+ * @Date 2020/3/6 22:14
+ * @Describe
+ * @Version 1.0
+ **/
+
+public class PmsSearchCrumb {
+
+    private String valueId;
+
+    private String valueName;
+
+    private String urlParam;
+
+    public String getValueId() {
+        return valueId;
+    }
+
+    public void setValueId(String valueId) {
+        this.valueId = valueId;
+    }
+
+    public String getValueName() {
+        return valueName;
+    }
+
+    public void setValueName(String valueName) {
+        this.valueName = valueName;
+    }
+
+    public String getUrlParam() {
+        return urlParam;
+    }
+
+    public void setUrlParam(String urlParam) {
+        this.urlParam = urlParam;
+    }
+}
